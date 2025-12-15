409eb3a2ee47c4ba5fca53beb2b57b8c65b41b04
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-15 22:34:07 +0530

    Accessing the character in string

---
+++ 3- Python String/AccessingCharofString.py
@@ -0,0 +1,13 @@
+# Accessing characters in String
+#Strings are indexed sequences. Positive indices start at 0 from the left; 
+# negative indices start at -1 from the right as
+
+print("Accessing String character")
+
+str1= "Python is easy to understand"
+#      01234567891011
+
+print(str1[0])
+print(str1[6])  #blank space also as index 
+print(str1[9])  #blank space also as index
+print(str1[10])
