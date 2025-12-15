dd3176b0feda19e36114e8a071c92c6098d123c7
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-15 23:02:44 +0530

    Real time Examples of Slicing String

---
+++ 3- Python String/RealExamplesSlicingString.py
@@ -0,0 +1,15 @@
+# Real Life usecase Examples Slicing String
+
+print("To get user name")
+email="user@gmail.com"
+print(email[:])  # print whole string 
+print(email[:4],"\n") # prints first 4 characters 
+
+print("To get file extension")
+file="filename.txt"
+print(file[-4:],"\n")
+
+print("Remive fisrt and Last character from the string")
+c="Python"
+print(c[:])
+print(c[1:5])
