f8796adbf81a5edb0ed01d2fe10cec4a7c9aa4ca
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-15 22:28:45 +0530

    initializing type's of string

---
+++ 3- Python String/Way'sTodefineString.py
@@ -0,0 +1,27 @@
+# Way's and Strings can be created using either single ('...') 
+# or double ("...") quotes. Both behave the same.
+
+str1='Here is single quotes'
+
+str2="Here is double quotes"
+
+str3=''' Here is 
+single triple 
+way to initialize'''
+str4=""" Here is
+another one in 
+double-triple  
+qoutes\n"""
+
+num =100
+str5= str(num)
+
+str6=input("Enter your name:")
+
+
+print(str1,"\n")
+print(str2,"\n")
+print(str3,"\n")
+print(str4,"\n")
+print(str5,"\n")
+print(str6,"\n")
