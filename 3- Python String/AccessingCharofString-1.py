3ec5977fc93db6d45605c072063f3c589f43fc44
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-15 22:38:33 +0530

    Accessing the character in string from the back side

---
+++ 3- Python String/AccessingCharofString-1.py
@@ -0,0 +1,8 @@
+# accessign character from the string by negative indexing 
+# means the access them from the back side of the string .
+
+str1="PythonTechnology"
+
+print(str1[0])  # first element of the string .
+print(str1[-1]) # last element of the string .
+print(str1[-3]) # last 3rd element of the string .
