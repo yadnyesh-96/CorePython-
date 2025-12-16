70213205dc187d414035aea69e6c66ff16bcbf7c
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-16 23:02:10 +0530

    Declaration methods of list in python

---
+++ 4- Python List/CreatingList.py
@@ -0,0 +1,17 @@
+
+# several ways to create list in python
+
+L1=[1,2,3,4,5]
+L2=['Apple',"Vivo",'Samsung','Oppo']
+L3=[1, 'Lenevo' , 2 , 'Acer', 3 , 'Hp' , 4 , 'Asus' ]  
+
+print(L1,"\n")
+print(L2,"\n")
+print(L3,"\n")
+
+# using list() constructor 
+a = list((1, 2, 3, 'apple', 4.5))  
+print(a)
+
+b = list("GFG")
+print(b)
