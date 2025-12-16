f6b74e64f820eb1d61ab20456aa35b0064fa0862
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-16 14:51:43 +0530

    Updating the character and word in string

---
+++ 1- Starting With Python/UpdatingString.py
@@ -0,0 +1,10 @@
+
+str1="python"
+
+str1= "P"+str1[1:] # updating the first character of string
+
+str2="THe Python is Programming Language"
+str3=str2.replace("THe","The") #replace the word
+print(str1,"\n")
+print(str2,"\n")
+print(str3,"\n")
