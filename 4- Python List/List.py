4efe717f947eb908dfbb5bce6d3068c17da48a7c
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-16 22:55:33 +0530

    New Chapter Python List

---
+++ 4- Python List/List.py
@@ -0,0 +1,12 @@
+#In Python, a list is a built-in data structure 
+# that can hold an ordered collection of items.
+
+#Can contain duplicate items
+#Mutable: items can be modified, replaced, or removed
+#Ordered: maintains the order in which items are added
+# Index-based: items are accessed using their position (starting from 0)
+# Can store mixed data types (integers, strings, booleans, even other lists)
+
+L1=[1,2,3]
+
+print(L1)
