70b1a8dff563a50403d2b89c9180c4204ffae214
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-15 22:45:19 +0530

    Loop's in python

--- 2- Loops/a-While Loop/SwapFirstAndLastdigit.py
+++ 2- Loops/a-While Loop/SwapFirstAndLastdigit.py
@@ -4,19 +4,19 @@ print("---- Swap First and Last digit of Number ----")
 
 num =int(input("Enter the Number : "))
 temp=num
-# count=0
-# while num!=0:
-#     rem=num%10
-#     num=num//10
-#     count+=1
+count=0
+while num!=0:
+    rem=num%10
+    num=num//10
+    count+=1
     
-# num=temp        #7894
+num=temp        #7894
 print("Before the swap first and Last digit: ",num)
-first_digit=num//(10**(count-1)) #  7
-last_digit=num%10                #  4
+# first_digit=num//(10**(count-1)) #  7
+# last_digit=num%10                #  4
 
-n=(num%(10**(count-1)))//10   # 89
+# n=(num%(10**(count-1)))//10   # 89
 
-swap1=last_digit*(10**(count-1)) # 4890
+# swap1=last_digit*(10**(count-1)) # 4890
 
 print("After the swap first and Last digit: ",swap1+(n*10)+first_digit)
