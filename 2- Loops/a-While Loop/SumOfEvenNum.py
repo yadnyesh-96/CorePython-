8bf8d88e0b05802e5465d2422b6bbe52d86e5d5a
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-15 22:44:59 +0530

    Loop's in python

--- 2- Loops/a-While Loop/SumOfEvenNum.py
+++ 2- Loops/a-While Loop/SumOfEvenNum.py
@@ -1,12 +1,5 @@
 """
-Q7.Write a Python program to find the sum of all even numbers 
-between 1 to n using a while loop.
-"""
-
-print("---- Sum of all Even Numbers 1 to n ----")
-
-n=int(input("Enter the Number:"))
-i=1
+Q
 sum=0;
 while i<=n:
         if i%2==0:
