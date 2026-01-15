03413309d30fed42fb561cdf7c30ed87c11983b3
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-15 22:44:32 +0530

    Loop's in python

--- 2- Loops/a-While Loop/CountDigits.py
+++ 2- Loops/a-While Loop/CountDigits.py
@@ -3,6 +3,14 @@ Q10.Write a Python program to count the number of digits in a
 number using a while loop.
 """
 
+print("---- Count Digit of Number ----")
+
+num=int(input("Enter the Number:"))
+
+print("The Digits in Given Number ",num)
+
+count=0
+
 if num==0:
 	print("Digits is :1")
 else:
