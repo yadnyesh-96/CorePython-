26e375106df280a1d058fb0936e6287465ad16f4
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-15 22:46:02 +0530

    Loop's in python

--- 2- Loops/b-for Loop/SumOfnNumbers.py
+++ 2- Loops/b-for Loop/SumOfnNumbers.py
@@ -1,3 +1,13 @@
 '''
 4. Print the sum of numbers from 1 to n
 '''
+
+print('Print the Sum of 1 to n')
+
+n=int(input('Enter the Number :'))
+
+sum=0
+for i in range(1,n+1):
+    sum+=i
+    
+print(sum)
