c317055b7e0abf75313942fc73fa14b74db25da2
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:45:30 +0530

    Review the List in Python

--- 4- Python List/List.py
+++ 4- Python List/List.py
@@ -2,7 +2,9 @@
 # that can hold an ordered collection of items.
 
 #Can contain duplicate items
-#Muta]ccessed using their position (starting from 0)
+#Mutable: items can be modified, replaced, or removed
+#Ordered: maintains the order in which items are added
+# Index-based: items are accessed using their position (starting from 0)
 # Can store mixed data types (integers, strings, booleans, even other lists)
 
 L1=[1,2,3]
