8456c52e7addaa598e1943f0f7585f85ce36bbaf
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:51:04 +0530

    Rview on the list in python

--- 4- Python List/Remove.py
+++ 4- Python List/Remove.py
@@ -1,6 +1,6 @@
 # remove(): Removes the first occurrence of an element.
 
-L=[20,20,25,30,35,40,45,50]
+L?=[20,20,25,30,35,40,45,50]
 print("before Remove the element in List =",L)
 L.remove(20)
 print("After Remove the element in List =",L)
