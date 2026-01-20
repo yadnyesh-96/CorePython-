85c8e9c1b28acf50ee68af2aa53b82c7cd5a5b38
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:42:30 +0530

    Review the List in Python

--- 4- Python List/methods_in_list.py
+++ 4- Python List/methods_in_list.py
@@ -1,4 +1,4 @@
-n = ["Amruta","Saket","Umesh","Sneha","Dipti","Sonali","Shital"]
+n = ["Amruta","Sanket","Umesh","Sneha","Dipti","Sonali","Shital"]
 #       0        1        2       3       4       5        6
 
 # list_name[start:stop:stepsize]
