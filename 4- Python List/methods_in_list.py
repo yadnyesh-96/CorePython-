5376cc9b0f4593f218091192a755cf6ac4be7577
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:40:56 +0530

    Review the List in Python

--- 4- Python List/methods_in_list.py
+++ 4- Python List/methods_in_list.py
@@ -1,7 +1,16 @@
-n = ["Amruta","Samket","Umesh","Sneha","Dipti","Sonali","Shotal"]
+n = ["Amruta","Samket","Umesh","Sneha","Dipti","Sonali","Shtal"]
 
 
 # list_name[start:stop:stepsize]
 print("1:",n)
 print("2:",n[2:5:2])
 print("3:",n[1::3])
+print("4:",n[3:5])
+
+#display sliced list
+print("5:",n[::2])
+
+#Display sliced list
+print("6:",n[::])
+
+
