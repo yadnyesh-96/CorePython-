3aabff1ccc326292263ef27fe15e5ef7f6832b72
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:50:47 +0530

    Rview on the list in python

--- 4- Python List/Remove.py
+++ 4- Python List/Remove.py
@@ -5,7 +5,7 @@ print("before Remove the element in List =",L)
 L.remove(20)
 print("After Remove the element in List =",L)
 
-value=L.pop(3)
+# value=L.pop(3)
 print(f"pop element is {value} and List is ={L}")
 
 del L[0] # delete the specific index valu
