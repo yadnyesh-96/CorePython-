526df14cafcfe8c9b8d2d5ce7489008d5eb7934d
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-20 21:51:11 +0530

    Rview on the list in python

--- 4- Python List/Remove.py
+++ 4- Python List/Remove.py
@@ -3,7 +3,7 @@
 L=[20,20,25,30,35,40,45,50]
 print("before Remove the element in List =",L)
 L.remove(20)
-print("After Remove the element in List =",L)
+# print("After Remove the element in List =",L)
 
 value=L.pop(3)
 print(f"pop element is {value} and List is ={L}")
