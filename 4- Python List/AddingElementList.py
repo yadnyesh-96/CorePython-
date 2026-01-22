d5b839cf0972ac457fde2fa4ee630e090a20422f
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-22 23:46:10 +0530

    List in Python

--- 4- Python List/AddingElementList.py
+++ 4- Python List/AddingElementList.py
@@ -11,9 +11,4 @@ l1=[10,20,30,40,50]
 print("List",l1)
 l1.append(60) #Adds an element at the end of the list.
 print("After using append() = ",l1)
-l1.extend([70,80,90,100]) #Adds multiple elements to the end of the list.
-print("After using extend() = ",l1)
-l1.insert(1,220)  #Adds an element at a specific position.
-print("After using insert() = ",l1) 
-l1.clear()
-print("After using clear() = ",l1) 
+
