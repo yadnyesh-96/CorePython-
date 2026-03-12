3d561f6fd4b877d63aab6c342359fefb1943689c
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-12 23:20:28 +0530

    reverse string manually

--- 06- String/revString.py
+++ 06- String/revString.py
@@ -1,4 +1,13 @@
 
 
 str = "Python";
-print(str[::-1])
+# print(str[::-1])
+
+def revrseString(s):
+    reversed_s=""
+    for char in s:
+        reversed_s=char+reversed_s
+    return reversed_s
+
+
+print(revrseString(str));
